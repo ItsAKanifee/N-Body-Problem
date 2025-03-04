a8189832e6d535205f5765715b4dc4fcf2f19c66
ItsAKanifee <ajaycd@icloud.com>
2025-03-03 21:20:20 -0600

    Changed where the radii get initialized in the process of the construction of the objects

--- Rework.js
+++ Rework.js
@@ -1,24 +1,28 @@
 class Body{
     
-    constructor(mass, pos, vel, color, planets, index){
+    constructor(mass, pos, vel, color, index){
         this.mass = mass;
         this.pos = pos;
         this.vel = vel;
         this.color = color;
 
         this.radii = [];
-        this.planets = planets;
         this.index = index;
-
-        for(let i = 0; i < planets.length(); i++){
-            this.radii.append([0,0]);
-        }
 
         this.acc = [0,0];
     }
 
+    set_planets(planets){
+        this.planets = planets;
+        
+        for(let i = 0; i < planets.length(); i++){
+            this.radii.append([this.pos[0] - planets[i].pos[0], this.pos[1] - planets[i].pos[1]]);   
+        }
+    }
+
 
     setGravity(){
+            
         
     }
 
